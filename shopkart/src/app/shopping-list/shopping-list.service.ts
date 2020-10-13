46fd6a11e795314a49705d63fcab87988ce43a5d
sashank12-hub <58355991+sashank12-hub@users.noreply.github.com>
2020-10-13 18:11:16 +0530

    g

--- shopkart/src/app/shopping-list/shopping-list.service.ts
+++ shopkart/src/app/shopping-list/shopping-list.service.ts
@@ -4,7 +4,7 @@ import { EventEmitter } from '@angular/core';
 export class ShoppingListService {
   ingredientsChanged = new EventEmitter<Ingredient[]>();
   private ingredients: Ingredient[] = [
-    new Ingredient('Apples', 6),
+    new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
   ];
 
